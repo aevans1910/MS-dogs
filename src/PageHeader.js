7f225d4e189da10f54fb626166c1c7175569b7f4
Ariane Evans <ariane.evans@student.makeschool.com>
2020-02-13 11:20:03 -0800

    first commit

---
+++ src/PageHeader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './PageHeader.css'
+
+function PageHeader() {
+    return (
+        <div className="PageHeader">
+            <header>
+                <h1>Dogs of Make School</h1>
+            </header>
+        </div>
+    )
+}
+
+export default PageHeader
