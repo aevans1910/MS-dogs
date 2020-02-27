07c0de59851eaa181650e4ef549e238a858d7398
Ariane Evans <ariane.evans@student.makeschool.com>
2020-02-27 11:16:45 -0800

    Updates

---
+++ precache-manifest.5b157c8798e80fe1a4a5da8c8ad532cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ca2c5cf8579c107507a7f4bf1595f2",
+    "url": "/MS-dogs/index.html"
+  },
+  {
+    "revision": "db221f70caecb7846549",
+    "url": "/MS-dogs/static/css/main.20522518.chunk.css"
+  },
+  {
+    "revision": "a3048b7fe33075c93c3f",
+    "url": "/MS-dogs/static/js/2.cc3bd1a7.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/MS-dogs/static/js/2.cc3bd1a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db221f70caecb7846549",
+    "url": "/MS-dogs/static/js/main.c824e79e.chunk.js"
+  },
+  {
+    "revision": "d9c284929f3825d638f7",
+    "url": "/MS-dogs/static/js/runtime-main.579e2014.js"
+  }
+]);
